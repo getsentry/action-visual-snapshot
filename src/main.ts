5cca1188fca3c319d2a967bf187f1b0a19465622
Billy Vong <billy@sentry.io>
2020-06-26 10:34:15 -0700

    make a bit more generic

--- src/main.ts
+++ src/main.ts
@@ -1,31 +1,31 @@
 /* eslint-env node */
-import * as fsNs from 'fs';
-import path from 'path';
-import * as github from '@actions/github';
-import * as core from '@actions/core';
-import {exec} from '@actions/exec';
-import {Storage} from '@google-cloud/storage';
-import {PNG} from 'pngjs';
-import pixelmatch from 'pixelmatch';
+import * as fsNs from "fs";
+import path from "path";
+import * as github from "@actions/github";
+import * as core from "@actions/core";
+import { exec } from "@actions/exec";
+import { Storage } from "@google-cloud/storage";
+import { PNG } from "pngjs";
+import pixelmatch from "pixelmatch";
 
 const fs = fsNs.promises;
-const {owner, repo} = github.context.repo;
-const token = core.getInput('githubToken');
+const { owner, repo } = github.context.repo;
+const token = core.getInput("githubToken");
 const octokit = github.getOctokit(token);
-const GITHUB_SHA = process.env.GITHUB_SHA || '';
-const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE || '';
-const GOOGLE_CREDENTIALS = process.env.GOOGLE_CREDENTIALS || '';
+const GITHUB_SHA = process.env.GITHUB_SHA || "";
+const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE || "";
+const GOOGLE_CREDENTIALS = process.env.GOOGLE_CREDENTIALS || "";
 
 const credentials = JSON.parse(
-  Buffer.from(GOOGLE_CREDENTIALS, 'base64').toString('utf8')
+  Buffer.from(GOOGLE_CREDENTIALS, "base64").toString("utf8")
 );
 
 // Creates a client
-const storage = new Storage({credentials});
+const storage = new Storage({ credentials });
 
 function isSnapshot(dirent: fsNs.Dirent) {
   // Only png atm
-  return dirent.isFile() && dirent.name.endsWith('.png');
+  return dirent.isFile() && dirent.name.endsWith(".png");
 }
 
 async function createDiff(
@@ -36,20 +36,23 @@ async function createDiff(
 ) {
   const [fileContent1, fileContent2] = await Promise.all([
     fs.readFile(file1),
-    fs.readFile(file2),
+    fs.readFile(file2)
   ]);
 
   const img1 = PNG.sync.read(fileContent1);
   const img2 = PNG.sync.read(fileContent2);
-  const {width, height} = img1;
-  const diff = new PNG({width, height});
+  const { width, height } = img1;
+  const diff = new PNG({ width, height });
 
   const result = pixelmatch(img1.data, img2.data, diff.data, width, height, {
-    threshold: 0.1,
+    threshold: 0.1
   });
 
   if (result > 0) {
-    await fs.writeFile(path.resolve(output, snapshotName), PNG.sync.write(diff));
+    await fs.writeFile(
+      path.resolve(output, snapshotName),
+      PNG.sync.write(diff)
+    );
   }
 
   return result;
@@ -57,13 +60,12 @@ async function createDiff(
 
 async function run(): Promise<void> {
   try {
-    const base: string = core.getInput('base');
-    const current: string = core.getInput('current');
-    const diff: string = core.getInput('diff');
-    core.debug(`${base} vs ${current} vs ${diff}`);
+    const current: string = core.getInput("snapshot-path");
+    const diff: string = core.getInput("diff-path");
+    core.debug(`${current} vs ${diff}`);
 
     core.debug(GITHUB_WORKSPACE);
-    core.setOutput('diff-path', diff);
+    core.setOutput("diff-path", diff);
 
     const newSnapshots = new Set<string>([]);
     const changedSnapshots = new Set<string>([]);
@@ -76,36 +78,37 @@ async function run(): Promise<void> {
     // artifacts from different workflows
     const {
       data: {
-        workflow_runs: [workflowRun],
-      },
+        workflow_runs: [workflowRun]
+      }
     } = await octokit.actions.listWorkflowRuns({
       owner,
       repo,
+      // Below is typed incorrectly
       // @ts-ignore
-      workflow_id: 'acceptance.yml',
-      branch: 'master',
+      workflow_id: core.getInput("base-workflow-id"),
+      branch: core.getInput("base-branch")
     });
 
     if (!workflowRun) {
-      core.debug('No workflow run found');
+      core.debug("No base workflow run found");
     }
 
     const {
-      data: {artifacts},
+      data: { artifacts }
     } = await octokit.actions.listWorkflowRunArtifacts({
       owner,
       repo,
-      run_id: workflowRun.id,
+      run_id: workflowRun.id
     });
 
     core.debug(JSON.stringify(artifacts));
     // filter artifacts for `visual-snapshots-main`
     const mainSnapshotArtifact = artifacts.find(
-      artifact => artifact.name === 'visual-snapshots-main'
+      artifact => artifact.name === core.getInput("base-artifact-name")
     );
 
     if (!mainSnapshotArtifact) {
-      core.debug('Artifact not found');
+      core.debug("Artifact not found");
       return;
     }
 
@@ -114,40 +117,43 @@ async function run(): Promise<void> {
       owner,
       repo,
       artifact_id: mainSnapshotArtifact.id,
-      archive_format: 'zip',
+      archive_format: "zip"
     });
 
     core.debug(JSON.stringify(download));
 
-    const outputPath = path.resolve('/tmp/visual-snapshots-base');
+    const outputPath = path.resolve("/tmp/visual-snapshots-base");
     try {
-      await fs.mkdir(outputPath, {recursive: true});
+      await fs.mkdir(outputPath, { recursive: true });
     } catch {
       core.debug(`Unable to create dir: ${outputPath}`);
     }
 
     await exec(
-      `curl -L -o ${path.resolve(outputPath, 'visual-snapshots-base.zip')} ${
+      `curl -L -o ${path.resolve(outputPath, "visual-snapshots-base.zip")} ${
         download.url
       }`
     );
     await exec(
-      `unzip -d ${outputPath} ${path.resolve(outputPath, 'visual-snapshots-base.zip')}`
+      `unzip -d ${outputPath} ${path.resolve(
+        outputPath,
+        "visual-snapshots-base.zip"
+      )}`
     );
 
     // read dirs
     const [currentDir, baseDir] = await Promise.all([
-      fs.readdir(current, {withFileTypes: true}),
+      fs.readdir(current, { withFileTypes: true }),
       fs.readdir(path.resolve(outputPath), {
-        withFileTypes: true,
-      }),
+        withFileTypes: true
+      })
     ]);
 
     // make output dir if not exists
     const diffPath = path.resolve(GITHUB_WORKSPACE, diff);
 
     try {
-      await fs.mkdir(diffPath, {recursive: true});
+      await fs.mkdir(diffPath, { recursive: true });
     } catch {
       core.debug(`Unable to create dir: ${diffPath}`);
     }
@@ -197,13 +203,13 @@ async function run(): Promise<void> {
     // TODO: Upload diff files where and update check with them
     const diffFiles =
       (await fs.readdir(diffPath, {
-        withFileTypes: true,
+        withFileTypes: true
       })) || [];
 
     const diffArtifactUrls = await Promise.all(
       diffFiles.filter(isSnapshot).map(async entry => {
         const [File] = await storage
-          .bucket('sentry-visual-snapshots')
+          .bucket("sentry-visual-snapshots")
           .upload(path.resolve(diffPath, entry.name), {
             // Support for HTTP requests made with `Accept-Encoding: gzip`
             destination: `${owner}/${repo}/${GITHUB_SHA}/diff/${entry.name}`,
@@ -215,14 +221,14 @@ async function run(): Promise<void> {
               // Enable long-lived HTTP caching headers
               // Use only if the contents of the file will never change
               // (If the contents will change, use cacheControl: 'no-cache')
-              cacheControl: 'public, max-age=31536000',
-            },
+              cacheControl: "public, max-age=31536000"
+            }
           });
         console.log(path.resolve(diffPath, entry.name), File);
 
         return {
           alt: entry.name,
-          image_url: `https://storage.googleapis.com/sentry-visual-snapshots/${File.name}`,
+          image_url: `https://storage.googleapis.com/sentry-visual-snapshots/${File.name}`
         };
       })
     );
@@ -230,21 +236,21 @@ async function run(): Promise<void> {
     console.log(diffArtifactUrls);
     const conclusion =
       !!changedSnapshots.size || !!missingSnapshots.size
-        ? 'failure'
+        ? "failure"
         : !!newSnapshots.size
-        ? 'neutral'
-        : 'success';
+        ? "neutral"
+        : "success";
 
     // Create a GitHub check with our results
     await octokit.checks.create({
       owner,
       repo,
-      name: 'Visual Snapshot',
+      name: "Visual Snapshot",
       head_sha: GITHUB_SHA,
-      status: 'completed',
+      status: "completed",
       conclusion,
       output: {
-        title: 'Visual Snapshots',
+        title: "Visual Snapshots",
         summary: `Summary:
 * **${changedSnapshots.size}** changed snapshots
 * **${missingSnapshots.size}** missing snapshots
@@ -252,16 +258,16 @@ async function run(): Promise<void> {
 `,
         text: `
 ## Changed snapshots
-${[...changedSnapshots].map(name => `* ${name}`).join('\n')}
+${[...changedSnapshots].map(name => `* ${name}`).join("\n")}
 
 ## Missing snapshots
-${[...missingSnapshots].map(([name]) => `* ${name}`).join('\n')}
+${[...missingSnapshots].map(([name]) => `* ${name}`).join("\n")}
 
 ## New snapshots
-${[...newSnapshots].map(name => `* ${name}`).join('\n')}
+${[...newSnapshots].map(name => `* ${name}`).join("\n")}
 `,
-        images: diffArtifactUrls,
-      },
+        images: diffArtifactUrls
+      }
     });
   } catch (error) {
     core.setFailed(error.message);
