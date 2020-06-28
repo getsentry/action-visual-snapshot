bb431fb772ab55edddbc3551b86a894fca4ed904
Billy Vong <billy@sentry.io>
2020-06-28 11:13:35 -0700

    fix stitching

--- src/main.ts
+++ src/main.ts
@@ -57,24 +57,25 @@ async function createDiff(
   });
 
   if (result > 0) {
-    const combined = new PNG({width: width * 3, height: height * 3});
+    const combinedWidth = width * 3;
+    const combined = new PNG({width: combinedWidth, height: height * 3});
 
     // original -> new -> diff
-    [img1, img2, diff].forEach((img, i) => {
-      for (let y = 0; y < height; y++) {
-        for (let x = 0; x < width; x++) {
-          const idx = (width * y + x) << 2;
-          const targetIdx = (width * y + x + i * width) << 2;
+    const images = [img1, img2, diff]; //.forEach((img, i) => {
 
-          // invert color
-          combined.data[targetIdx] = img.data[idx];
-          combined.data[targetIdx + 1] = img.data[idx + 1];
-          combined.data[targetIdx + 2] = img.data[idx + 2];
-          combined.data[targetIdx + 3] = img.data[idx + 3];
-        }
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < combinedWidth; x++) {
+        const idx = (width * y + (x % width)) << 2;
+        const targetIdx = (width * y + x) << 2;
+        const img = images[x / width];
+
+        // invert color
+        combined.data[targetIdx] = img.data[idx];
+        combined.data[targetIdx + 1] = img.data[idx + 1];
+        combined.data[targetIdx + 2] = img.data[idx + 2];
+        combined.data[targetIdx + 3] = img.data[idx + 3];
       }
-      img.data;
-    });
+    }
 
     await fs.writeFile(
       path.resolve(output, snapshotName),
