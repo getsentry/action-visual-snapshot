ba0572710cc5d45c7fc3b3563880e6fd44b725ee
Billy Vong <billy@sentry.io>
2020-07-03 11:57:40 -0700

    fix: fix path to index.ejs

--- src/util/generateImageGallery.ts
+++ src/util/generateImageGallery.ts
@@ -1,13 +1,18 @@
 import {promises as fs} from 'fs';
+import path from 'path';
+
 import ejs from 'ejs';
 
 export async function generateImageGallery(
   target: string,
   data: {changed: Record<string, string>}
 ) {
-  const html = ejs.render(await fs.readFile('./index.ejs', 'utf8'), {
-    images: JSON.stringify(data),
-  });
+  const html = ejs.render(
+    await fs.readFile(path.resolve('./index.ejs'), 'utf8'),
+    {
+      images: JSON.stringify(data),
+    }
+  );
 
   await fs.writeFile(target, html);
 }
