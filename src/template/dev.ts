02251902656459435afd5cdc27d825aacd49b696
Billy Vong <billy@sentry.io>
2020-07-09 14:41:40 -0700

    add example gallery

---
+++ src/template/dev.ts
@@ -0,0 +1,38 @@
+import path from 'path';
+
+import {build} from './build';
+
+import {generateImageGallery} from '../util/generateImageGallery';
+
+async function main() {
+  await build();
+  await generateImageGallery(
+    path.resolve(__dirname, '../../example_gallery/index.html'),
+    {
+      changed: {
+        'project-ownership': 'acceptance/project-ownership.png',
+        'project-settings-all-integrations':
+          'acceptance/project-settings-all-integrations.png',
+        'project-settings-debug-symbols':
+          'acceptance/project-settings-debug-symbols.png',
+        'project-settings-general-settings':
+          'acceptance/project-settings-general-settings.png',
+        'project-settings-release-tracking':
+          'acceptance/project-settings-release-tracking.png',
+        'project-settings-service-hooks-create':
+          'acceptance/project-settings-service-hooks-create.png',
+        'project-settings-service-hooks-details':
+          'acceptance/project-settings-service-hooks-details.png',
+        'project-settings-service-hooks-empty-list':
+          'acceptance/project-settings-service-hooks-empty-list.png',
+        'project-settings-service-hooks-list-with-entries':
+          'acceptance/project-settings-service-hooks-list-with-entries.png',
+        'project-settings-tags-after-remove':
+          'acceptance/project-settings-tags-after-remove.png',
+        'project-settings-tags': 'acceptance/project-settings-tags.png',
+      },
+    }
+  );
+}
+
+main();
