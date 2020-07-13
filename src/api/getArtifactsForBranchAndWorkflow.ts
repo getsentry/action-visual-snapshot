be0e5b5f30f2aa8b4b96f53ea28b1a4ecf3832b5
Billy Vong <billy@sentry.io>
2020-07-13 15:48:04 -0700

    this is not well documented imo

--- src/api/getArtifactsForBranchAndWorkflow.ts
+++ src/api/getArtifactsForBranchAndWorkflow.ts
@@ -65,9 +65,7 @@ export async function getArtifactsForBranchAndWorkflow(
       per_page: 100,
     }
   )) {
-    console.log(response);
-    // This is typed wrong... probably because of paginate()?
-    const workflowRuns = response.data.workflow_runs;
+    const workflowRuns = response.data;
 
     if (!workflowRuns.length) {
       core.warning(`Workflow ${workflow_id} not found in branch ${branch}`);
