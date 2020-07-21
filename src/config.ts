80d60852dfc3081183829c3c87cf14a9cf75f9c5
Billy Vong <billy@sentry.io>
2020-07-21 13:51:04 -0700

    fix: fix api endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // TODO make this configurable
 export const API_ENDPOINT =
-  'https://sentry-visual-snapsot-dwunkkvj6a-uc.a.run.app/api';
+  'https://sentry-visual-snapshot-dwunkkvj6a-uc.a.run.app/api';
 export const SENTRY_DSN =
   'https://6b971d11c2af4b468105f079294e372c@o1.ingest.sentry.io/5324467';
