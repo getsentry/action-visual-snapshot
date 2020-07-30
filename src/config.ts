099e6fc5aa6378054eb92861696e1fbd338e0e2d
Billy Vong <billy@sentry.io>
2020-07-30 08:00:11 -0700

    feat: Allow custom API endpoint

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,7 @@
-// TODO make this configurable
+import * as core from '@actions/core';
+
 export const API_ENDPOINT =
+  core.getInput('api-endpoint') ||
   'https://sentry-visual-snapshot-dwunkkvj6a-uc.a.run.app/api';
 export const SENTRY_DSN =
   'https://6b971d11c2af4b468105f079294e372c@o1.ingest.sentry.io/5324467';
