d855fd998c9e3ef5e308a204b5ca4ce0622a0c06
Billy Vong <billy@sentry.io>
2020-08-04 09:35:44 -0700

    feat: Ignore `Unprocessable Entity` when uploading

--- src/util/saveSnapshots.ts
+++ src/util/saveSnapshots.ts
@@ -1,3 +1,4 @@
+import * as core from '@actions/core';
 import * as glob from '@actions/glob';
 import * as artifact from '@actions/artifact';
 
@@ -10,15 +11,24 @@ export async function saveSnapshots({
   artifactName,
   rootDirectory,
 }: SaveSnapshotsParams) {
-  const artifactClient = artifact.create();
-  const currentGlobber = await glob.create(`${rootDirectory}/**/*.png`, {
-    followSymbolicLinks: false,
-  });
-  const files = await currentGlobber.glob();
+  try {
+    const artifactClient = artifact.create();
+    const currentGlobber = await glob.create(`${rootDirectory}/**/*.png`, {
+      followSymbolicLinks: false,
+    });
+    const files = await currentGlobber.glob();
 
-  return await artifactClient.uploadArtifact(
-    artifactName,
-    files,
-    rootDirectory
-  );
+    return await artifactClient.uploadArtifact(
+      artifactName,
+      files,
+      rootDirectory
+    );
+  } catch (err) {
+    core.warning(err.message);
+    if (!err.message.includes('Unprocessable Entity')) {
+      // throw err;
+    }
+
+    return null;
+  }
 }
