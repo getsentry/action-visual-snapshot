3ecb328ed0767baccd51e8ac720fa0b89100d65d
Billy Vong <billyvg@users.noreply.github.com>
2020-11-17 16:55:57 -0800

    feat: Add Sentry performance monitoring (#24)

--- src/util/createDiff.ts
+++ src/util/createDiff.ts
@@ -1,6 +1,7 @@
 import {promises as fs} from 'fs';
 import path from 'path';
 import {PNG} from 'pngjs';
+import * as Sentry from '@sentry/node';
 
 import {PixelmatchOptions} from '@app/types';
 
@@ -17,6 +18,11 @@ export async function createDiff(
   file2: string,
   pixelmatchOptions?: PixelmatchOptions
 ) {
+  const transaction = Sentry.getCurrentHub().getScope()?.getTransaction();
+  const span = transaction?.startChild({
+    op: 'createDiff',
+    description: snapshotName,
+  });
   const {result, diff} = await getDiff(file1, file2, pixelmatchOptions);
 
   if (result > 0) {
@@ -26,5 +32,6 @@ export async function createDiff(
     );
   }
 
+  span?.finish();
   return result;
 }
