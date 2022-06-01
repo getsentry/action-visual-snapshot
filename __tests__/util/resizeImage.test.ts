8192b15c979be20019daae29203bce51dddbb5fe
Jonas <jonas@badalic.com>
2022-06-01 09:54:28 -0400

    perf(resize): faster resize operation (#61)
    No idea how often we have to resize the images, but we can improve the performance of the current resize by just initializing the buffer to the appropriate size and copying rows of pixels instead of px by px.
    
    Quick benchmark
    ```
    px copy 920x500 -> 1980x1080 x 139 ops/sec ±0.27% (81 runs sampled)
    row copy 920x500 -> 1980x1080 x 839 ops/sec ±10.21% (44 runs sampled)
    px copy 4x3 -> 1980x1080 x 159 ops/sec ±0.12% (83 runs sampled)
    row copy 4x3 -> 1980x1080 x 2,049 ops/sec ±17.84% (36 runs sampled)
    ```
    
    I manually tested resizing a few photos just to verify.
    <details>
      <summary markdown="span">cat</summary>
      new (height)
    
    ![cat height new](https://user-images.githubusercontent.com/9317857/171272469-96e542c9-f26b-4192-ac8b-9ed8ef6e051d.png)
    old (height)
    ![cat height old](https://user-images.githubusercontent.com/9317857/171272471-2cf10499-ee62-4f5d-a625-42e14cd6f5fb.png)
    original
    ![cat](https://user-images.githubusercontent.com/9317857/171272472-10d5a368-63ac-46d7-9990-a410d93776ae.png)
    </details>
    <details>
      <summary markdown="span">4x3</summary>
    
    new resize (height)
    ![4x3 height new](https://user-images.githubusercontent.com/9317857/171271796-e25fbee2-be1d-4069-b1c9-02442b122121.png)
    old resize (height)
    ![4x3 height old](https://user-images.githubusercontent.com/9317857/171271797-7f40c64d-435f-447b-ab89-bf65caf1cb31.png)
    new resize (width+height)
    ![4x3 new](https://user-images.githubusercontent.com/9317857/171271799-fce0a31b-dca1-44d3-8856-d4496f23a87c.png)
    old resize (width+height)
    ![4x3 old](https://user-images.githubusercontent.com/9317857/171271801-6d17e571-386c-4ef3-9e7b-ada745394c7d.png)
    
    </details>

--- __tests__/util/resizeImage.test.ts
+++ __tests__/util/resizeImage.test.ts
@@ -1,127 +1,39 @@
+import {PNG} from 'pngjs';
 import {resizeImage} from '@app/util/resizeImage';
 
-const PNG_DATA = [
-  1,
-  1,
-  1,
-  0.5,
-  2,
-  2,
-  2,
-  0.5,
-  3,
-  3,
-  3,
-  0.5,
-  4,
-  4,
-  4,
-  0.5,
-  5,
-  5,
-  5,
-  0.5,
-  6,
-  6,
-  6,
-  0.5,
-];
+const makeTestImage = () => {
+  const image = new PNG({width: 2, height: 1});
 
-const mockPngConstructor = jest.fn((opts: any) => ({
-  data: {},
-  height: opts.height,
-  width: opts.width,
-}));
+  const BYTES_PER_PIXEL = 4;
+  const data = Buffer.allocUnsafe(image.width * image.height * BYTES_PER_PIXEL);
 
-jest.mock('pngjs', () => {
-  // eslint-disable-next-line
-  function PNG(opts: any) {
-    return mockPngConstructor(opts);
+  for (let i = 0; i < data.length; i += BYTES_PER_PIXEL) {
+    data[i] = 255;
+    data[i + 1] = 128;
+    data[i + 2] = 64;
+    data[i + 3] = 32;
   }
-  PNG.sync = {
-    write: jest.fn(() => 1),
-    read: jest.fn(() => ({
-      width: 3,
-      height: 2,
-      data: PNG_DATA,
-    })),
-  };
 
-  return {PNG};
-});
+  image.data = data;
+  return image;
+};
 
-describe('resizeImage', function() {
-  it('diffs two equal sized images and creates a combined image', async function() {
-    const oldImage = {width: 3, height: 2, data: PNG_DATA};
-    // @ts-ignore
-    const newImage = resizeImage(oldImage, 4, 3);
+describe('resizeImage', () => {
+  it('adds column', () => {
+    const resized = resizeImage(makeTestImage(), 3, 1);
 
-    // new image
-    expect(mockPngConstructor).toHaveBeenCalledWith({width: 4, height: 3});
+    expect(resized.width).toBe(3);
+    expect(resized.data.slice(8, 12)).toEqual(Buffer.from([0, 0, 0, 0]));
+  });
+  it('adds row', () => {
+    const resized = resizeImage(makeTestImage(), 2, 2);
 
-    // eslint-disable-next-line
-    expect(newImage).toEqual({
-      data: {
-        // Original first row
-        '0': 1,
-        '1': 1,
-        '2': 1,
-        '3': 0.5,
-        '4': 2,
-        '5': 2,
-        '6': 2,
-        '7': 0.5,
-        '8': 3,
-        '9': 3,
-        '10': 3,
-        '11': 0.5,
+    expect(resized.height).toBe(2);
+    expect(resized.data.slice(8, 12)).toEqual(Buffer.from([0, 0, 0, 0]));
+  });
+  it('preserves old data', () => {
+    const resized = resizeImage(makeTestImage(), 3, 2);
 
-        // Additional column added (width 3 --> 4)
-        '12': 0,
-        '13': 0,
-        '14': 0,
-        '15': 0,
-
-        // Original 2nd row
-        '16': 4,
-        '17': 4,
-        '18': 4,
-        '19': 0.5,
-        '20': 5,
-        '21': 5,
-        '22': 5,
-        '23': 0.5,
-        '24': 6,
-        '25': 6,
-        '26': 6,
-        '27': 0.5,
-
-        // Additional column added (width 3 --> 4)
-        '28': 0,
-        '29': 0,
-        '30': 0,
-        '31': 0,
-
-        // Additional row (height 2->3)
-        '32': 0,
-        '33': 0,
-        '34': 0,
-        '35': 0,
-        '36': 0,
-        '37': 0,
-        '38': 0,
-        '39': 0,
-        '40': 0,
-        '41': 0,
-        '42': 0,
-        '43': 0,
-        '44': 0,
-        '45': 0,
-        '46': 0,
-        '47': 0,
-      },
-      width: 4,
-      height: 3,
-    });
+    expect(resized.data.slice(0, 8)).toEqual(makeTestImage().data.slice(0, 8));
   });
 });
