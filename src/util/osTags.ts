a51e6deb828cf071eacd030750fd13c94167e514
JonasBa <jonas@badalic.com>
2022-06-01 16:41:08 -0400

    feat(sentry): add freemem tag

--- src/util/osTags.ts
+++ src/util/osTags.ts
@@ -4,5 +4,6 @@ export function getOSTags(): Record<string, string> {
   return {
     'os.cpus.total': os.cpus().length.toFixed(0),
     'os.memory.total': os.totalmem().toFixed(0),
+    'os.memory.free': os.freemem().toFixed(0),
   };
 }
