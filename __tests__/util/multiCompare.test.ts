c09a6489b453e16817dbd7d937fcc130c8ed1a60
JonasBa <jonas@badalic.com>
2022-06-10 10:46:11 -0400

    test(diffs): compare old with new functionality

---
+++ __tests__/util/multiCompare.test.ts
@@ -0,0 +1,19 @@
+import {multiCompare} from '@app/util/multiCompare';
+
+describe('multiCompare', function () {
+  it('does a 3 way diff', async () => {
+    const result = await multiCompare({
+      snapshotName: 'test.png',
+      baseHead: './__tests__/util/diffSnapshots/imgs/base/acceptance/test.png',
+      branchBase:
+        './__tests__/util/diffSnapshots/imgs/mergeBase/acceptance/test.png',
+      branchHead:
+        './__tests__/util/diffSnapshots/imgs/current/acceptance/test.png',
+      outputDiffPath: './__tests__/util/diffSnapshots/imgs/diff',
+      outputMergedPath: './__tests__/util/diffSnapshots/imgs/merged',
+    });
+
+    // Hardcoded nb of diffed pixels
+    expect(result).toBe(16120);
+  });
+});
