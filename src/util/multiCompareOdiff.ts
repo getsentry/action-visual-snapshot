b76c72d8524b02fdd36889c5c69926c7171173d9
JonasBa <jonas@badalic.com>
2022-06-10 10:43:25 -0400

    feat(diffs): new diffing utils

---
+++ src/util/multiCompareOdiff.ts
@@ -0,0 +1,79 @@
+import sharp from 'sharp';
+import {readFileSync, unlinkSync} from 'fs';
+import path from 'path';
+
+import {getDiffOdiff} from './getDiffOdiff';
+
+type Options = {
+  snapshotName: string;
+  branchBase: string;
+  baseHead: string;
+  branchHead: string;
+  outputDiffPath: string;
+  outputMergedPath: string;
+};
+
+export async function multiCompareODiff({
+  snapshotName,
+  baseHead,
+  branchBase,
+  branchHead,
+  outputDiffPath,
+  outputMergedPath,
+}: Options): Promise<number> {
+  const outputPath = path.resolve(outputDiffPath, snapshotName);
+  const outputMergedMaskPathA = path.resolve(
+    outputMergedPath,
+    snapshotName.replace('.png', '.a.png')
+  );
+  const outputMergedMaskPathB = path.resolve(
+    outputMergedPath,
+    snapshotName.replace('.png', '.b.png')
+  );
+
+  const {result: diffB} = await getDiffOdiff(
+    baseHead,
+    branchHead,
+    outputMergedMaskPathB,
+    {
+      outputDiffMask: true,
+      antialiasing: true,
+    }
+  );
+
+  // Hot path for when baseHead and branchBase do not have any respective changes,
+  // we can indue that there is nothing to merge and we can skip the 2nd image diff operation
+  if (diffB === 0) {
+    return 0;
+  }
+
+  const {result: diffA} = await getDiffOdiff(
+    baseHead,
+    branchBase,
+    outputMergedMaskPathA,
+    {
+      outputDiffMask: true,
+      antialiasing: true,
+    }
+  );
+
+  const maskA = readFileSync(outputMergedMaskPathA);
+  const maskB = readFileSync(outputMergedMaskPathB);
+
+  const finalMask = await sharp(maskA)
+    .composite([{input: maskB, blend: 'xor'}])
+    .toBuffer();
+
+  unlinkSync(outputMergedMaskPathA);
+  unlinkSync(outputMergedMaskPathB);
+
+  const result = Math.abs(diffB - diffA);
+
+  if (result > 0) {
+    await sharp(readFileSync(baseHead))
+      .composite([{input: finalMask, blend: 'over'}])
+      .toFile(outputPath);
+  }
+
+  return result;
+}
