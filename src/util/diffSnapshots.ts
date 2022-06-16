24f1f0ad60b7a7dc7abfabdf50c2e81fe86c32ec
JonasBa <jonas@badalic.com>
2022-06-16 14:41:38 -0400

    ref(odiff): update tests and cleanup message type

--- src/util/diffSnapshots.ts
+++ src/util/diffSnapshots.ts
@@ -224,12 +224,12 @@ export async function diffSnapshots({
       if (mergeBaseSnapshots.has(file)) {
         promise = workerPool
           .enqueue({
+            file,
             branchBase: path.resolve(mergeBasePath, file),
             baseHead,
             branchHead,
             outputDiffPath,
             outputMergedPath,
-            snapshotName: file,
             diffOptions,
           })
           .then(onSuccess)
